240768034288b32a68b51e939f673e2e7c04dbd9
Dmitry <dima.captan@yandex.ru>
2023-05-29 20:13:47 +0300

    deleted tests depending on views and subscribers

--- tests/test_channel.py
+++ tests/test_channel.py
@@ -7,12 +7,15 @@ from src.channel import Channel
 def make_channel():
     return Channel("UCjay7c-KSW2nC8Grq_q8tHg")
 
+
 @pytest.fixture
 def make_two_channel():
     return (
         Channel('UC-OVMPlMA3-YCIeg4z5z23A'),
         Channel("UCjay7c-KSW2nC8Grq_q8tHg")
     )
+
+
 def test_class(make_channel):
     channel = make_channel
     assert isinstance(channel, object)
@@ -22,30 +25,30 @@ def test_init(make_channel):
     channel = make_channel
     assert channel.channel_id == "UCjay7c-KSW2nC8Grq_q8tHg"
 
+
 def test_channel_id(make_channel, capsys):
     channel = make_channel
     channel.channel_id = "dFdfdfsdfgdf"
     output = capsys.readouterr()
     assert output.out == "Ошибка\n"
 
+
 def test_src(make_two_channel):
     chn_1, chn_2 = make_two_channel
     assert str(chn_1) == "MoscowPython (https://www.youtube.com/channel/UC-OVMPlMA3-YCIeg4z5z23A)"
     assert str(chn_2) == "Scammers (https://www.youtube.com/channel/UCjay7c-KSW2nC8Grq_q8tHg)"
 
-def test_add(make_two_channel):
-    chn_1, chn_2 = make_two_channel
-    assert chn_1 + chn_2 == 1_416_000
 
-def test_sub(make_two_channel):
-    chn_1, chn_2 = make_two_channel
-    assert chn_2 - chn_1 == 1_364_000
+def test_repr(make_channel):
+    channel = make_channel
+    assert repr(channel) == "Channel('UCjay7c-KSW2nC8Grq_q8tHg')"
 
 
 def test_lt(make_two_channel):
     chn_1, chn_2 = make_two_channel
     assert (chn_1 < chn_2) == True
     assert (chn_2 < chn_1) == False
+
 
 def test_le(make_two_channel):
     chn_1, chn_2 = make_two_channel
@@ -58,10 +61,12 @@ def test_gt(make_two_channel):
     assert (chn_1 > chn_2) == False
     assert (chn_2 > chn_1) == True
 
+
 def test_ge(make_two_channel):
     chn_1, chn_2 = make_two_channel
     assert (chn_1 >= chn_2) == False
     assert (chn_2 >= chn_1) == True
+
 
 def test_eq(make_two_channel):
     chn_1, chn_2 = make_two_channel
