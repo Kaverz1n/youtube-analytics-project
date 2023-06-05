96823837b58b92194490bc951ea1273a1e677a1c
Dmitry <dima.captan@yandex.ru>
2023-06-05 17:05:28 +0300

    added exception

--- src/video.py
+++ src/video.py
@@ -12,11 +12,18 @@ class Video:
         self.video_id = video_id
 
         data = self.__get_data()
-        self.title = data['items'][0]['snippet']['title']
-        self.url = f'https://youtu.be/{self.video_id}'
-        self.view_count = int(data['items'][0]['statistics']['viewCount'])
-        self.like_count = int(data['items'][0]['statistics']['likeCount'])
-        self.duration = self.__get_duration()
+        try:
+            self.title = data['items'][0]['snippet']['title']
+            self.url = f'https://youtu.be/{self.video_id}'
+            self.view_count = int(data['items'][0]['statistics']['viewCount'])
+            self.like_count = int(data['items'][0]['statistics']['likeCount'])
+            self.duration = self.__get_duration()
+        except IndexError:
+            self.title = None
+            self.url = None
+            self.view_count = None
+            self.like_count = None
+            self.duration = None
 
     def __str__(self):
         return f'{self.title}'
@@ -61,5 +68,3 @@ class PLVideo(Video):
     def __init__(self, video_id, playlist_id):
         super().__init__(video_id)
         self.playlist_id = playlist_id
-
-
